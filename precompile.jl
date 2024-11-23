e937e2c104d10e6c681b16c8e9e49f00df3b8615
Li-Yuan Chiang <64157474+FishboneChiang@users.noreply.github.com>
2024-11-23 01:31:33 -0500

    precompile

--- precompile.jl
+++ precompile.jl
@@ -1,2 +1,2 @@
 using PackageCompiler
-create_sysimage(["QML", "JSON3", "Dates"], sysimage_path="packages.so", precompile_execution_file="precompile_packages.jl")
+create_sysimage(["QML", "JSON3", "Dates"], sysimage_path="packages.so")
---
+++ run_app.sh
@@ -0,0 +1,1 @@
+julia --sysimage=packages.so main.jl
